b37e25c30744860e23e0b4c7c90dfbf234821f96
zibredlektab <zibredlektab@gmail.com>
2025-03-20 18:03:18 -0400

    AKS sheet displays nicely

--- heightcalc/lib/main.dart
+++ heightcalc/lib/main.dart
@@ -23,34 +23,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
       ),
-      //darkTheme: ThemeData.dark(),
-      //themeMode: ThemeMode.system,
-      //initialRoute: '/',
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       routes: {
         '/config': (context) => const ConfigPage(),
       }
     );
   }
 }
-
-/*
-typedef HeadEntry = DropdownMenuEntry<Head>;
-enum Head {
-  2575('2575', 16),
-  arrihead('ArriHead', 22),
-
-  final String label;
-  final int headHeight;
-
-  static final List<HeadEntry> entries = UnmodifiableListView<HeadEntry>(
-    values.map<HeadEntry>(
-      (Head headHeight) => HeadEntry(
-        value: headHeight,
-        label: label,
-      )
-    )
-  )
-}*/
 
 class TripodHead {
   final String label;
@@ -78,6 +58,7 @@ class _HomePageState extends State<HomePage> {
 
   int _goalHeight = 0;
   TripodHead? _selectedHead = heads.first;
+  final TextEditingController _headMenuController = TextEditingController();
 
   void _calculate(int newHeight) {
     print ('new height is $newHeight');
@@ -86,84 +67,124 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void setHead(TripodHead newHead) {
+  void _setHead(TripodHead newHead) {
+    print ('new head is ${newHead.label}');
+    setState(() {
+      _selectedHead = newHead;
+    });
+  }
 
+  void _showAKSSheet() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Center(
+          child: Container(
+            padding: EdgeInsets.all(15),
+            child: ListView(
+              children:[
+                Center(
+                  child: Column(
+                    children:[
+                      Text("Required Accessories", style: Theme.of(context).textTheme.headlineMedium!),
+                      Gap(5),
+                      Text("Select the accessories you intend to use for this setup.", textAlign: TextAlign.center,),
+                      Text("foo"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    );
   }
 
   @override
   Widget build(BuildContext context) {
 
-    final TextEditingController textEditingController = TextEditingController();
 
-    return Scaffold(
-      appBar: AppBar(title: const Text('Height Calc')),
-      body: Center(
-        child: Column(
-          children:[
-            Gap(10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children:[
-                Text("Desired lens height: "),
-                SizedBox(
-                  width: 100,
-                  child: TextFormField(
-                    initialValue:'0',
-                    decoration: InputDecoration(border: OutlineInputBorder(),labelText: 'inches',),
-                    autocorrect: false,
-                    autofocus: true,
-                    enableSuggestions: false,
-                    enableInteractiveSelection: false,
-                    keyboardType: TextInputType.number,
-                    onChanged: (String? inputValue) async {
-                      int newHeight = 0;
-                      inputValue ??= "0"; // protect against null cases
-                      try {
-                        newHeight = int.parse(inputValue);
-                      } catch(e) {
-                        print('invalid number provided');
-                      }
-                      _calculate(newHeight);
-                    },
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Height Calc')),
+        body: Center(
+          child: Column(
+            children:[
+              Gap(10),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children:[
+                  Text("Desired lens height: "),
+                  SizedBox(
+                    width: 100,
+                    child: TextFormField(                    
+                      decoration: InputDecoration(border: OutlineInputBorder(), hintText: "0", helperText: "inches",),
+                      autocorrect: false,
+                      autofocus: true,
+                      canRequestFocus: true,
+                      enableSuggestions: false,
+                      enableInteractiveSelection: false,
+                      keyboardType: TextInputType.number,
+                      onChanged: (String? inputValue) {
+                        int newHeight = 0;
+                        inputValue ??= "0"; // protect against null cases
+                        try {
+                          newHeight = int.parse(inputValue);
+                        } catch(e) {
+                          print('invalid number provided');
+                        }
+                        _calculate(newHeight);
+                      },
+                    ),
                   ),
-                ),
-              ]
-            ),
-            Gap(10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children:[
-                Text("Current head: "),
-                DropdownMenu<TripodHead>(
-                  initialSelection: heads.first,
-                  controller: textEditingController,
-                  requestFocusOnTap: true,
-                  label: Text('${_selectedHead?.label}'),
-                  onSelected: (TripodHead? newHead) {
-                    newHead ??= heads.first;
-                    setState(() {
-                      _selectedHead = newHead;
-                    });
-                  },
-                  dropdownMenuEntries:
-                    heads.map<DropdownMenuEntry<TripodHead>>((TripodHead head) {
-                      return DropdownMenuEntry<TripodHead>(
-                        label: head.label,
-                        value: head,);
-                    }).toList(),
-                )
-              ]
-            ),
-            Gap(10),
-            Text('$_goalHeight inches to lens, ${_selectedHead?.label} head in use'),
-          ],
+                ]
+              ),
+              Gap(10),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children:[
+                  Text("Current head: "),
+                  DropdownMenu<TripodHead>(
+                    initialSelection: heads.first,
+                    hintText: "Select a head",
+                    controller: _headMenuController,
+                    requestFocusOnTap: true,
+                    enableFilter: true,
+                    //label: const Text("Head"),
+                    inputDecorationTheme: InputDecorationTheme(
+                      
+                    ),
+                    onSelected: (TripodHead? newHead) {
+                      newHead ??= heads.first;
+                      _setHead(newHead);
+                    },
+                    dropdownMenuEntries:
+                      heads.map<DropdownMenuEntry<TripodHead>>((TripodHead head) {
+                        return DropdownMenuEntry<TripodHead>(
+                          label: head.label,
+                          value: head,);
+                      }).toList(),
+                  )
+                ]
+              ),
+              Gap(10),
+              ElevatedButton(
+                onPressed: () { _showAKSSheet(); },
+                child: Text("AKS"),
+              ),
+              Gap(10),
+              Text('$_goalHeight inches to lens, ${_selectedHead?.label} head in use'),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          navKey.currentState?.pushNamed('/config');
-        },
-        child: const Text('Config'),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            navKey.currentState?.pushNamed('/config');
+          },
+          child: const Text('Config'),
+        ),
       ),
     );
   }
