a84cf427c2209048bcbf8afc380d1252c6466fa4
zibredlektab <zibredlektab@gmail.com>
2025-03-20 15:17:24 -0400

    Beginnings of a ui layout

--- heightcalc/lib/main.dart
+++ heightcalc/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:ffi';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +14,6 @@ final GlobalKey<NavigatorState> navKey = GlobalKey<NavigatorState>();
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,33 +27,141 @@ class MyApp extends StatelessWidget {
       //themeMode: ThemeMode.system,
       //initialRoute: '/',
       routes: {
-        '/second': (context) => Scaffold(
-          appBar: AppBar(
-            title: const Text('Second Page'),
-          ),
-          body: const Center(
-            child: Text('Baz Bat'),
-          ),
-        ),
+        '/config': (context) => const ConfigPage(),
       }
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+/*
+typedef HeadEntry = DropdownMenuEntry<Head>;
+enum Head {
+  2575('2575', 16),
+  arrihead('ArriHead', 22),
+
+  final String label;
+  final int headHeight;
+
+  static final List<HeadEntry> entries = UnmodifiableListView<HeadEntry>(
+    values.map<HeadEntry>(
+      (Head headHeight) => HeadEntry(
+        value: headHeight,
+        label: label,
+      )
+    )
+  )
+}*/
+
+class TripodHead {
+  final String label;
+  final int height; // Mitchell to camera base
+  
+  TripodHead(this.label, this.height);
+
+  String getLabel() {
+    return label;
+  }
+}
+
+List<TripodHead> heads = [
+  TripodHead('2575', 16),
+  TripodHead('ArriHead', 22),
+];
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _goalHeight = 0;
+  TripodHead? _selectedHead = heads.first;
+
+  void _calculate(int newHeight) {
+    print ('new height is $newHeight');
+    setState(() {
+      _goalHeight = newHeight;
+    });
+  }
+
+  void setHead(TripodHead newHead) {
+
+  }
+
+  @override
   Widget build(BuildContext context) {
+
+    final TextEditingController textEditingController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(title: const Text('Height Calc')),
-      body: Column(
-        children:[
-          
-        ],
+      body: Center(
+        child: Column(
+          children:[
+            Gap(10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children:[
+                Text("Desired lens height: "),
+                SizedBox(
+                  width: 100,
+                  child: TextFormField(
+                    initialValue:'0',
+                    decoration: InputDecoration(border: OutlineInputBorder(),labelText: 'inches',),
+                    autocorrect: false,
+                    autofocus: true,
+                    enableSuggestions: false,
+                    enableInteractiveSelection: false,
+                    keyboardType: TextInputType.number,
+                    onChanged: (String? inputValue) async {
+                      int newHeight = 0;
+                      inputValue ??= "0"; // protect against null cases
+                      try {
+                        newHeight = int.parse(inputValue);
+                      } catch(e) {
+                        print('invalid number provided');
+                      }
+                      _calculate(newHeight);
+                    },
+                  ),
+                ),
+              ]
+            ),
+            Gap(10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children:[
+                Text("Current head: "),
+                DropdownMenu<TripodHead>(
+                  initialSelection: heads.first,
+                  controller: textEditingController,
+                  requestFocusOnTap: true,
+                  label: Text('${_selectedHead?.label}'),
+                  onSelected: (TripodHead? newHead) {
+                    newHead ??= heads.first;
+                    setState(() {
+                      _selectedHead = newHead;
+                    });
+                  },
+                  dropdownMenuEntries:
+                    heads.map<DropdownMenuEntry<TripodHead>>((TripodHead head) {
+                      return DropdownMenuEntry<TripodHead>(
+                        label: head.label,
+                        value: head,);
+                    }).toList(),
+                )
+              ]
+            ),
+            Gap(10),
+            Text('$_goalHeight inches to lens, ${_selectedHead?.label} head in use'),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          navKey.currentState?.pushNamed('/second');
+          navKey.currentState?.pushNamed('/config');
         },
         child: const Text('Config'),
       ),
@@ -57,88 +169,16 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class ConfigPage extends StatelessWidget {
+  const ConfigPage({super.key});
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: const Text('Config')),
+      body: ListView(
+        children: [Text('foo'),],
+      )
     );
   }
 }
